80f081138caa7b72729be8ad0a405d9548c77ea3
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2025-12-29 13:57:32 +0700

    fix : answer Q2 : complete

--- question-2.js
+++ question-2.js
@@ -7,3 +7,10 @@ const students = [
 ];
 
 // เริ่มเขียนโค้ดตรงนี้
+const totalScore = students
+  .filter((student) => student.score > 50) // 1. กรองคะแนน > 50
+  .map((student) => student.score * 1.1)   // 2. เพิ่มคะแนน 10% (*1.1)
+  .reduce((sum, score) => sum + score, 0); // 3. หาผลรวม
+
+// 4. แสดงผลโดยใช้ Template Literal
+console.log(`Total score is ${totalScore}`);
